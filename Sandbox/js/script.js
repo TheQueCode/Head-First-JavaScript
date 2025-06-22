e51c49291901a18bba11a65e73279de3b6c92bed
TheQueCode <thequecode@gmail.com>
2025-06-22 16:16:59 -0500

    sandbox

---
+++ Sandbox/js/script.js
@@ -0,0 +1,19 @@
+let scoops = 5;
+
+while (scoops > 0) {
+  console.log("Another scoop!");
+  scoops--;  
+}
+
+console.log("Life without ice cream isn't the same");
+
+let cashInWallet = 20;
+
+if (cashInWallet > 5) {
+  order = `I'll take a cheeseburger and fries`;
+} else {
+  order = `I'll just have a glass of water`;
+}
+
+console.log(order);
+
