9e5ae4620b8056c71aab5e838f19f667b53efde1
TheQueCode <thequecode@gmail.com>
2025-06-24 17:40:19 -0500

    sandbox

--- Project-BattleShip/js/script.js
+++ Project-BattleShip/js/script.js
@@ -11,7 +11,7 @@ let guesses = 0;
 let isSunk = false;
 
 while (isSunk == false) {
-  guess = prompt('Ready, aim, fire! (Enter a number from 0-6!): ');
+  //guess = prompt('Ready, aim, fire! (Enter a number from 0-6!): ');
   if (guess < 0 || guess > 6) {
     console.log("Please enter a valid cell number!");
   } else {
--- Sandbox/js/script.js
+++ Sandbox/js/script.js
@@ -29,3 +29,37 @@ bark('rover', 23);
 bark('spot', 13);
 bark('spike', 53);
 bark('lady', 17);
+
+function bake (degrees) {
+  let message;
+
+  if (degrees > 500) {
+    message = `I'm not a nuclear reactor!`;
+  } else if (degrees < 100) {
+    message = `I'm not a refrigerator!`;
+  } else {
+    message = `That's a very comfortable temperature for me. Thank you.`;
+  }
+  return message;
+}
+let status = bake(350);
+
+console.log(bake(425));
+
+
+
+const radius = 5.2;
+
+function calculateArea (radius) {
+  let area;
+  if (radius <= 0) {
+    return 0;
+  } else {
+    area = Math.PI * radius * radius;
+    return area;
+  }
+}
+
+let theArea = calculateArea(radius);
+
+console.log(`The area is ${theArea}.`);
