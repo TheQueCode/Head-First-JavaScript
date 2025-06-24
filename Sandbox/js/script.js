fd8c158799f7f414f576f21b2b5a306b5d34d28a
TheQueCode <thequecode@gmail.com>
2025-06-23 23:41:53 -0500

    sandbox function

--- Sandbox/js/script.js
+++ Sandbox/js/script.js
@@ -17,3 +17,15 @@ if (cashInWallet > 5) {
 
 console.log(order);
 
+function bark (name, weight) {
+  if (weight > 20) {
+    console.log(`${name} says WOOF WOOF!`);
+  } else {
+    console.log(`${name} says woof woof!`);
+  }
+}
+
+bark('rover', 23);
+bark('spot', 13);
+bark('spike', 53);
+bark('lady', 17);
