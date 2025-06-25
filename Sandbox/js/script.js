1f37c8f5ec58e42efd770ae79a6e6d3eb5d8a58b
TheQueCode <thequecode@gmail.com>
2025-06-25 09:53:48 -0500

    thing-a-ma-jig

--- Sandbox/js/script.js
+++ Sandbox/js/script.js
@@ -106,3 +106,35 @@ letsSee("Brad");
 console.log(points); // 1111
 // console.log(message); // Error: message not defined
 
+
+function clunk (times) {
+  let num = times;
+  while (num > 0) {
+    display('clunk');
+    num--;
+  }
+}
+
+function thingamajig (size) {
+  let facky = 1;
+  clunkCounter = 0;
+  if (size == 0) {
+    display('clank');
+  } else if (size == 1) {
+    display('thunk');
+  } else {
+    while (size > 1) {
+      facky = facky * size;
+      size--;
+    }
+    clunk(facky);
+  }
+}
+
+function display (output) {
+  console.log(output);
+  clunkCounter++;
+}
+let clunkCounter = 0;
+thingamajig();
+console.log(clunkCounter);
