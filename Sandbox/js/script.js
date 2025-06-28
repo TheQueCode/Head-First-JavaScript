4ae156b57938760c6b5501305e1fc0b0d8949155
TheQueCode <thequecode@gmail.com>
2025-06-27 23:20:13 -0500

    bubble solution

--- Sandbox/js/script.js
+++ Sandbox/js/script.js
@@ -172,3 +172,11 @@ function makePhrases () {
 }
 
 makePhrases();
+
+let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
+
+for (let i = 0; i < scores.length; i++){
+  let output = `Bubble Solution #: ${i} Score: ${scores[i]}`;
+  console.log(output);
+}
+
